cbd9dc340422c4cda4bbec028f0ca78e65465607
rinov <rinov@rinov.com>
2025-03-18 12:48:42 +0700

    manifest added

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Material Management',
+    'version': '1.0',
+    'summary': 'Module untuk mengelola material',
+    'author': 'Rinov',
+    'category': 'Inventory',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/material_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
